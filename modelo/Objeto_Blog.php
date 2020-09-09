c60d4acdaffdd45050d7e638c340bb2caacba20a
ltorrico <leo.f.t@hotmail.com>
2020-09-09 01:30:26 -0300

    Comienzo de Proyecto Blog y creacion de vista y modelo

---
+++ modelo/Objeto_Blog.php
@@ -0,0 +1,86 @@
+<?php
+
+    class Objeto_Blog{
+
+        // Propiedades del objeto Blog
+
+        private $id;
+
+        private $Titulo;
+
+        private $Fecha;
+
+        private $Comentario;
+
+        private $Imagen;
+
+
+        //Metodos de acceso getters y setters
+
+        public function getId(){
+
+            return $this->id;
+
+        }
+
+        public function setId($id){
+
+            $this->id=$id;
+
+        }
+
+        public function getTitulo(){
+
+            return $this->Titulo;
+
+        }
+
+        public function setTitulo($Titulo){
+
+            $this->Titulo=$Titulo;
+
+        }
+
+        public function getFecha(){
+
+            return $this->Fecha;
+
+        }
+
+        public function setFecha($Fecha){
+
+            $this->Fecha=$Fecha;
+
+        }
+
+        public function getComentario(){
+
+            return $this->Comentario;
+
+        }
+
+        public function setComentario($Comentario){
+
+            $this->Comentario=$Comentario;
+
+        }
+
+        public function getImagen(){
+
+            return $this->Imagen;
+
+        }
+
+        public function setImagen($Imagen){
+
+            $this->Imagen=$Imagen;
+
+        }
+
+    }
+
+
+
+
+
+?>
